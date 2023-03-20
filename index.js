6ed672c804d3e34c14aed48c83a6ceebe6736b70
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-03-20 17:37:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,24 +27,21 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
-      var tom1 = new Audio("sounds/tom-1.mp3");
-
     case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-      var tom2 = new Audio("sounds/tom-2.mp3");
-
     case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
-      var tom3 = new Audio('sounds/tom-3.mp3');
-
     case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
-      var tom4 = new Audio('sounds/tom-4.mp3');
 
     case "j":
       var snare = new Audio('sounds/snare.mp3');
